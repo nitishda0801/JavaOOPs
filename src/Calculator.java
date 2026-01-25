3e7f02d866746a1fd07f38c68b516df297f52348
nitishda4433 <nitishkr08011997@gmail.com>
2026-01-26 00:32:10 +0530

    commit calculator implemented

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 public class Calculator {
 
 
     public void subtract(int a, int b){
         System.out.println("Diff of "+a+" and "+b+" is: "+ (a-b));
+    }
+    public void mod(int a, int b){
+        System.out.println("Mod of "+a+" and "+b+" is: "+ (a%b));
     }
     public void addition(int a, int b){
         System.out.println("Sum of "+a+" and "+b+" is: "+ (a+b));
@@ -15,14 +22,38 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-
-        System.out.println("Calculator: ");
-
+        Scanner sc = new Scanner(System.in);
         Calculator c = new Calculator();
-        c.addition(10,2);
-        c.subtract(10,2);
-        c.division(10,2);
-        c.multiply(10,2);
+        System.out.println("Calculator: ");
+        while (true){
+            System.out.println("1. add 2. sub 3.Div 4.Mul 5 Mod 0. exit");
+            int op = sc.nextInt();
+            if(op == 0) exit(0);
+            System.out.println("Enter first number: ");
+            int a = sc.nextInt();
+            System.out.println("Enter second number: ");
+            int b = sc.nextInt();
+            switch (op){
+                case 1:
+                    c.addition(a,b);
+                    break;
+                case 2:
+                    c.subtract(a,b);
+                    break;
+                case 3:
+                    c.division(a,b);
+                    break;
+                case 4:
+                    c.multiply(a,b);
+                    break;
+                case 5:
+                    c.mod(a,b);
+                    break;
+                default:
+                    System.out.println("Thanks!");
+            }
+        }
+
 
     }
 }
