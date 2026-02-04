0462afa5202145d6d3fce567abfac021a4b6eba2
nitishda4433 <nitishkr08011997@gmail.com>
2026-02-05 02:10:09 +0530

    commit calculator implemented

---
+++ src/javarelationship/JavaRelationship.java
@@ -0,0 +1,39 @@
+package javarelationship;
+class Address{
+    //example of HAs-A relationship, where class Student has a class Address.
+    String city;
+    String state;
+
+    Address(String city1,String state1){
+        city = city1;
+        state=state1;
+    }
+    public void getAddressInfo(){
+        System.out.println("Address is: "+city +", "+state);
+    }
+}
+class Student{
+     String name;
+     int rollno;
+
+     Address address = new Address("Kolkata","West Bengal");
+
+     Student(String name1, int rollno1){
+         name = name1;
+         rollno = rollno1;
+     }
+
+     void displayInfo(){
+         System.out.println("Name: "+name+", Roll no: "+rollno);
+         address.getAddressInfo();
+     }
+
+}
+
+public class JavaRelationship {
+    public static void main(String[] args) {
+        Student student = new Student("Nitish",433);
+        student.displayInfo();
+
+    }
+}
