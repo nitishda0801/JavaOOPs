cecb979e5e8de3070889e17a9c78e3151a6e7462
nitishda4433 <nitishkr08011997@gmail.com>
2026-04-05 23:56:22 +0530

    commit collection framework

--- src/collections/SetInterfaces.java
+++ src/collections/SetInterfaces.java
@@ -1,6 +1,8 @@
 package collections;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class SetInterfaces {
     public static void main(String[] args) {
@@ -9,5 +11,21 @@ public class SetInterfaces {
         set.add("nitish");
         set.add(10.3);
         System.out.println(set);
+        Iterator itr = set.iterator();
+        while (itr.hasNext()){
+            System.out.println(itr.next());
+        }
+        System.out.println(set.contains("nitish"));
+
+        TreeSet treeSet = new TreeSet(); //stores same data-type or Homogeneous type data
+
+        treeSet.add(10);
+        treeSet.add(5);
+        treeSet.add(6);
+        treeSet.add(2);
+//        treeSet.add("name");// stores same data type only
+//        treeSet.add(null); can't store null values, it compares each value internally to store in sorted order
+        System.out.println(treeSet);
+
     }
 }
